1bfd8a4516a05a1f10d902b848ed7a6a637e6a9c
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-11 18:22:16 +0530

    Navigation Click Drawer Close

--- app/src/main/java/com/example/crap/MainActivity.java
+++ app/src/main/java/com/example/crap/MainActivity.java
@@ -2,17 +2,21 @@ package com.example.crap;
 
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawer;
 
@@ -24,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawer = findViewById(R.id.drawer);
         Button btn = findViewById(R.id.button);
-
+        setNavigationViewListener();
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,5 +41,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+            case R.id.settings: {
+                Toast.makeText(getApplicationContext(), "Setting", Toast.LENGTH_LONG).show();
+                break;
+            }case R.id.payments_details: {
+                Toast.makeText(getApplicationContext(), "Payment Details", Toast.LENGTH_LONG).show();
+                break;
+            }case R.id.about: {
+                Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_LONG).show();
+                break;
+            }case R.id.logout: {
+                Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
+        //close navigation drawer
+        mDrawer.closeDrawer(Gravity.START);
+        return true;
+    }
+
+    public void setNavigationViewListener() {
+        NavigationView navigationView = findViewById(R.id.loopNav);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
