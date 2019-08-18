0ac26a27d91828404fe4983d6ef55af0712b7847
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-18 21:24:05 +0530

    Removed Landscape Layout XML and make application only Portrait

--- app/src/main/java/com/example/crap/utils/FontChanger.java
+++ app/src/main/java/com/example/crap/utils/FontChanger.java
@@ -1,6 +1,9 @@
 package com.example.crap.utils;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 import com.example.crap.R;
 
@@ -17,5 +20,49 @@ public class FontChanger extends Application {
                 .setDefaultFontPath("fonts/Raleway-Medium.ttf")
                 .setFontAttrId(R.attr.fontPath).build()
         );
+
+        // register to be informed of activities starting up
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            @Override
+            public void onActivityCreated(Activity activity,
+                                          Bundle savedInstanceState) {
+
+                // new activity created; force its orientation to portrait
+                activity.setRequestedOrientation(
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
     }
 }
