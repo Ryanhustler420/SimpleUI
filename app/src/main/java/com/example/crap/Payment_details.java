0aee6dadad1863ae794c7b38ec47f078a9003c9e
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-19 13:41:50 +0530

    fix onBackPressed animation

--- app/src/main/java/com/example/crap/Payment_details.java
+++ app/src/main/java/com/example/crap/Payment_details.java
@@ -26,7 +26,6 @@ public class Payment_details extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onBackPressed();
-                overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left);
             }
         });
     }
@@ -39,5 +38,6 @@ public class Payment_details extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left);
     }
 }
